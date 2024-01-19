2fb5845fe56e389588b352bc669c78c07278d131
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-01-20 03:59:09 +0700

    fixed a bug

--- src/components/HueSlider/styles/styles.ts
+++ src/components/HueSlider/styles/styles.ts
@@ -59,18 +59,33 @@ export const usesHueSliderLayout = (options?: OrientationableOptions) => {
     return style({
         ...usesRangeLayout(), // copy from <Range>
         ...style({
+            // animations:
+            filter: 'none !important',
+            
+            
+            
             ...children(trackElm, {
+                // animations:
+                filter: 'none !important',
+                
+                
+                
                 // backgrounds:
                 ...ifParentOrientationInline({
-                    backgroundImage: [[
-                        'linear-gradient(90deg in hsl increasing hue, hsl(0, 100%, 50%), hsl(359.95, 100%, 50%))',
+                    backgroundImage  : [[
+                        // 'linear-gradient(90deg in hsl increasing hue, hsl(0, 100%, 50%), hsl(359.95, 100%, 50%))',
+                        "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAWgAAAABCAYAAADzaTTzAAAAAXNSR0IArs4c6QAAAFpJREFUOE/lk1EKgDAMQ59B739g7SZjDEp/BBmbzI9QQqANpNmyyBzA7rAwT4ILOKmzYQY35yPej77e6obgjwHPDHbgY8me0y3V9pWO9e6tx309uUgfaO64gG9YrbgDh794DAAAAABJRU5ErkJggg==')",
                     ]],
+                    backgroundRepeat : 'repeat-y',
                 }),
                 ...ifParentOrientationBlock({
-                    backgroundImage: [[
-                        'linear-gradient(0deg in hsl increasing hue, hsl(0, 100%, 50%), hsl(359.95, 100%, 50%))',
+                    backgroundImage  : [[
+                        // 'linear-gradient(0deg in hsl increasing hue, hsl(0, 100%, 50%), hsl(359.95, 100%, 50%))',
+                        "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAFoCAYAAACWmZGvAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsIAAA7CARUoSoAAAAB2SURBVEhL7ZSxDoAgEEPLIf//wRBqIUYlMLngcMvLtRxLUwARacCBAWmU7rGhSlgBYLmhTw92eauDXXsNCCCNUVKhDUjuTR6F3iyUG3mU7jUADLSoH8uU3BoKd5pW+LinTq9v/M+joFopOiV3QY/1Ld3rQA0oJ+UcwNCeq9/TAAAAAElFTkSuQmCC')",
                     ]],
+                    backgroundRepeat : 'repeat-x',
                 }),
+                backgroundSize   : 'contain',
                 
                 
                 
@@ -82,6 +97,11 @@ export const usesHueSliderLayout = (options?: OrientationableOptions) => {
                     background: 'none',
                 }),
                 ...children(thumbElm, {
+                    // animations:
+                    filter: 'none !important',
+                    
+                    
+                    
                     backgroundColor: `hsl(calc(${rangeVars.valueRatio} * 360), 100%, 50%)`,
                 }),
             }),
